2fca193a3fcb3f5cc78fd63be56ae3711e01a0e8
yasserbelhaddad <103585329+yasserbelhaddad@users.noreply.github.com>
2024-06-01 13:40:07 +0100

    Update eureka_config.py

--- api/eureka_config.py
+++ api/eureka_config.py
@@ -6,7 +6,7 @@ from py_eureka_client.eureka_client import EurekaClient
 async def start_eureka_client():
     # Initialize Eureka client
     eureka_client = EurekaClient(
-        eureka_server="https://ntic-discovery-server2.onrender.com/eureka/",
+        eureka_server="https://ntic-discovery-server.onrender.com/eureka/",
         app_name="price_prediction",
         instance_port=8000,  # The port your Django app is running on
         instance_ip="127.0.0.1",  # The IP address your Django app is running on
